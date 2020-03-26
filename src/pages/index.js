1116be368b1e25f7666abdbfd2a4fb7ef70f2932
Jeremiah Dala <mrdala2010@yahoo.com>
2020-03-26 14:07:08 -0400

    add README to initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `purple`, fontSize: `72px` }}>Welcome to Clandestine!</div>
+)
