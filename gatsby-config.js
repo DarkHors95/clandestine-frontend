fae7df7c7624473b239a546aa6cf5b8f417bc18b
Jeremiah Dala <mrdala2010@yahoo.com>
2020-03-28 20:01:57 -0400

    changed navbars

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,22 @@
  */
 
 module.exports = {
-  /* Your site config here */
-}
+  // Add common site data (not required by the theme, but recommended)
+  siteMetadata: {
+    title: "Clandestine",
+    description: "",
+    siteUrl: "https://github.com/DarkHors95/clandestine-frontend"
+  },
+  plugins: [
+    {
+      resolve: "gatsby-theme-musician",
+      // More info about the theme options:
+      options: {
+        // These are default values that you can override
+        // basePath: `/`,
+        // content: `content`,
+        // pwaName: null
+      }
+    }
+  ]
+};
--- src/pages/about.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default () => (
-  <div style={{ color: `teal` }}>
-    <h1>About Clandestine</h1>
-    <p>Clandestine is a music sharing app for anonymous sharing of music with friends and having a leader-board for most listened recommendations</p>
-  </div>
-)
